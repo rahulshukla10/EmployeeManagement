a3c2d85b23b76189376abbf8ffe857b125fbc4b1
Rahul Shukla <rsrahulshukla365@gmail.com>
2023-10-20 14:11:27 +0530

    Refactor : Added the validation to employeedto.cs

--- EmployeeManagement/Model/EmployeeDto.cs
+++ EmployeeManagement/Model/EmployeeDto.cs
@@ -1,12 +1,25 @@
-﻿namespace EmployeeManagement.Model
+﻿using System.ComponentModel.DataAnnotations;
+using System.Net;
+
+namespace EmployeeManagement.Model
 {
     public class EmployeeDto
     {
-        public int Id { get; set; }
-        public string? FirstName { get; set; }
-        public string? LastName { get; set; }
-        public string? Email { get; set; }
+        public int EmployeeId { get; set; }
+
+        [Required(ErrorMessage = "First name is required.")]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "Last name is required.")]
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage = "Email is required.")]
+        [EmailAddress(ErrorMessage = "Invalid email address.")]
+        public string Email { get; set; }
+
+        [Range(18, 99, ErrorMessage = "Age must be between 18 and 99.")]
         public int Age { get; set; }
+
         public string? Address { get; set; }
     }
 }
